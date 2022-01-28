1ede3075b48196e71fdc93a06894135f8ea568af
Shigni <Louisservant86@gmail.com>
2022-01-28 13:49:59 +0100

    modal confirmation

--- Js/modal.js
+++ Js/modal.js
@@ -22,3 +22,18 @@ function closeModal() {
 }
 closeBtn[0].addEventListener('click', closeModal);
 
+const modalSubmit = document.getElementsByClassName('container-confirmation-submit');
+const closeModalSubmit = document.getElementsByClassName('close-modal-submit');
+const closeBtnConfirmation = document.getElementById('close-btn-confirmation');
+const modalForm = document.getElementById('form');
+
+// Display confirmation
+function displayModalSubmit() {
+    const modalBody = document.querySelector('.modal-body')
+    modalForm.style.display = 'none';
+    let div = document.createElement('div')
+    div.className = ('modal-confirmation')
+    div.innerHTML = '<p>Nous avons bien pris en compte votre inscription<br><br>à bientôt !</p>';
+    modalBody.appendChild(div);
+}
+
